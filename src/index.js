f08f87cfe45497f58d7f022a24cd5ece5b1705a1
Vivian Le <vivianhdle@gmail.com>
2019-03-26 12:40:19 -0700

    started prototype

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(),
     document.getElementById('root')
 );
+
+
+const luckyNumber = () => (Math.floor(Math.random*1000)+1);
+
+const greeting = props => {
+    <div className="container">
+        <h1>Welcome {props.name}</h1>
+        <h2 className="text-muted">Your lucky number is:{luckyNumber()}</h2>
+    </div>
+}
